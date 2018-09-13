e7f77833a7a4db6d734c81d0c7723eddbcbbcd0d
John Colella <colella.john@gmail.com>
2018-09-13 07:37:03 -0400

    Add uuid field to User

--- graphql/schema/queries/fields/user/index.js
+++ graphql/schema/queries/fields/user/index.js
@@ -7,13 +7,13 @@ const UserType = require('../../../types/user/userType');
 exports.user = {
   type: UserType,
   args: {
-    id: {
+    uuid: {
       type: GraphQLID,
     }
   },
   resolve: (root, params, ctx) => {
-    if (params.id) {
-      return User.where({ id: params.id }).fetch().then(user => (
+    if (params.uuid) {
+      return User.where({ uuid: params.uuid }).fetch().then(user => (
         user.attributes
       ));
     }
